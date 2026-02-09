23ba88fc7138b6346daacdf68acc62a3cf91f707
RostovGeorgiy <perast201208@gmail.com>
2026-02-09 19:46:19 +0700

    Changed getSideLength() arguments order.

--- Shapes/src/ru/academits/rostov/shapes/Triangle.java
+++ Shapes/src/ru/academits/rostov/shapes/Triangle.java
@@ -80,8 +80,8 @@ public class Triangle implements Shape {
         return getLength(y1, y2, y3);
     }
 
-    private static double getSideLength(double x1, double x2, double y1, double y2) {
-        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+    private static double getSideLength(double x2, double x1, double y2, double y1) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
     @Override
