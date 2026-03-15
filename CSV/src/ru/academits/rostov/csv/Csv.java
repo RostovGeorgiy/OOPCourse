97b9d7a147c5d02ec65eb95494cc91827fbbfb79
RostovGeorgiy <perast201208@gmail.com>
2026-03-15 12:32:00 +0700

    Used BufferedReader, removed StringBuilder and ArrayList usage, moved logic to different class, fixed formatting, fixed errors, added distinct File not found message.

--- CSV/src/ru/academits/rostov/csv/Csv.java
+++ CSV/src/ru/academits/rostov/csv/Csv.java
@@ -1,78 +1,116 @@
 package ru.academits.rostov.csv;
 
-import java.util.ArrayList;
+import java.io.*;
 
 public class Csv {
-    public static String convertHtmlEscapeCharacters(String line) {
-        StringBuilder stringbuilder = new StringBuilder(line.length());
+    private static final String INDENT = "    ";
 
-        for (int i = 0; i < line.length(); ++i) {
-            if (line.charAt(i) == '<') {
-                stringbuilder.append("&lt;");
-            } else if (line.charAt(i) == '>') {
-                stringbuilder.append("&gt;");
-            } else if (line.charAt(i) == '&') {
-                stringbuilder.append("&amp;");
-            } else {
-                stringbuilder.append(line.charAt(i));
+    public static void convertCsvToHtml(String inputFilePath, String outputFilePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
+             PrintWriter writer = new PrintWriter(outputFilePath)) {
+
+            writer.println("""
+                    <!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>CSV to HTML.</title>
+                    </head>
+                    <body>
+                    <h1>HTML file containing table converted from CSV file.</h1>
+                    <table border="1">""");
+
+            boolean isLineBreak = false;
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                if (line.isEmpty()) {
+                    continue;
+                }
+
+                int quotesAmount = 0;
+
+                String newLine = System.lineSeparator();
+
+                if (!isLineBreak) {
+                    writer.print(INDENT + "<tr>");
+                    writer.print(newLine + INDENT.repeat(2) + "<td>");
+                }
+
+                for (int i = 0; i < line.length(); ++i) {
+                    if (line.charAt(i) == '<') {
+                        writer.print("&lt;");
+                    } else if (line.charAt(i) == '>') {
+                        writer.print("&gt;");
+                    } else if (line.charAt(i) == '&') {
+                        writer.print("&amp;");
+                    } else if (line.charAt(i) == '"') {
+                        quotesAmount++;
+
+                        if (i == line.length() - 1) {
+                            if (quotesAmount % 2 == 0 && isLineBreak) {
+                                writer.print("<br>");
+                            } else {
+                                writer.print("</td>" + newLine);
+                                writer.println(INDENT + "</tr>");
+                                isLineBreak = false;
+                            }
+
+                            quotesAmount = 0;
+                        } else if (line.charAt(i + 1) == '"') {
+                            writer.print(line.charAt(i));
+                            ++i;
+                            quotesAmount++;
+                        }
+                    } else if (line.charAt(i) == ',') {
+                        if (i == 0) {
+                            writer.println("</td>");
+                            writer.print(INDENT.repeat(2) + "<td>");
+                        } else if (quotesAmount % 2 != 0) {
+                            writer.print(line.charAt(i));
+
+                            if (i == line.length() - 1) {
+                                writer.print("<br>");
+                                isLineBreak = true;
+                            }
+                        } else if (i == line.length() - 1 && !isLineBreak) {
+                            writer.println("</td>" + newLine + INDENT.repeat(2) + "<td>" + "</td>");
+                            writer.println(INDENT + "</tr>");
+                            quotesAmount = 0;
+                        } else {
+                            writer.print("</td>" + newLine);
+                            writer.print(INDENT.repeat(2) + "<td>");
+                            quotesAmount = 0;
+                        }
+                    } else if (i == line.length() - 1) {
+                        writer.print(line.charAt(i));
+
+                        if (!isLineBreak && quotesAmount % 2 == 0) {
+                            writer.print("</td>" + newLine);
+                            writer.println(INDENT + "</tr>");
+
+                            quotesAmount = 0;
+                        } else {
+                            writer.print("<br>");
+                            isLineBreak = true;
+                        }
+                    } else {
+                        writer.print(line.charAt(i));
+                    }
+                }
             }
+
+            writer.print("""
+                    </table>
+                    </body>
+                    </html>""");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("An exception has occurred: " + e.getMessage());
         }
-
-        return stringbuilder.toString();
-    }
-
-    public static ArrayList<String> convertFileLine(String line, boolean isLineBreak) {
-        line = convertHtmlEscapeCharacters(line);
-
-        ArrayList<String> result = new ArrayList<>();
-
-        int quotesAmount = 0;
-
-        String newLine = System.lineSeparator();
-
-        if (!isLineBreak) {
-            result.add("    <tr>");
-            result.add(newLine + "        <td>");
-        }
-
-        for (int i = 0; i < line.length(); ++i) {
-            if (line.charAt(i) == '"') {
-                quotesAmount++;
-
-                if (i == line.length() - 1) {
-                    result.add("</td>" + newLine);
-                    result.add("    </tr>");
-                    quotesAmount = 0;
-                } else if (line.charAt(i + 1) == '"') {
-                    result.add(String.valueOf(line.charAt(i)));
-                    ++i;
-                    quotesAmount++;
-                }
-            } else if (line.charAt(i) == ',') {
-                if (quotesAmount % 2 == 0) {
-                    result.add("</td>" + newLine);
-                    result.add("        <td>");
-                    quotesAmount = 0;
-                } else {
-                    result.add(String.valueOf(line.charAt(i)));
-                }
-            } else if (i == line.length() - 1) {
-                result.add(String.valueOf(line.charAt(i)));
-
-                if (!isLineBreak && quotesAmount % 2 == 0) {
-                    result.add("</td>" + newLine);
-                    result.add("    </tr>");
-
-                    quotesAmount = 0;
-                } else {
-                    result.add("<br>");
-                    isLineBreak = false;
-                }
-            } else {
-                result.add(String.valueOf(line.charAt(i)));
-            }
-        }
-
-        return result;
     }
 }
--- CSV/src/ru/academits/rostov/csv_main/Main.java
+++ CSV/src/ru/academits/rostov/csv_main/Main.java
@@ -2,60 +2,12 @@ package ru.academits.rostov.csv_main;
 
 import ru.academits.rostov.csv.Csv;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
-        String inputFilePath = args[0];
-        String outputFilPath = args[1];
-
-        try (Scanner scanner = new Scanner(new FileInputStream(inputFilePath));
-             PrintWriter writer = new PrintWriter(outputFilPath)) {
-
-            writer.println("""
-                    <!DOCTYPE html>
-                    <html lang = "en">
-                    <head>
-                        <meta charset="UTF-8">
-                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                        <title>CSV to HTML.</title>
-                    </head>
-                    <body>
-                    <h1>HTML file containing table converted from CSV file.</h1>
-                    <table border="1">""");
-
-            ArrayList<String> result = new ArrayList<>();
-
-            boolean isLineBreak = false;
-
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-
-                result.addAll(Csv.convertFileLine(line, isLineBreak));
-
-                if (result.getLast().equals("    </tr>")) {
-                    writer.println(String.join("", result));
-
-                    isLineBreak = false;
-
-                    result.clear();
-                } else {
-                    isLineBreak = true;
-                }
-            }
-
-            writer.print("""
-                    </table>
-                    </body>
-                    </html>""");
-
-        } catch (FileNotFoundException e) {
-            System.out.println("An exception has occurred: " + e.getMessage());
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Program takes 2 arguments: path to source CSV file, and path to destination HTML file.");
         }
+
+        Csv.convertCsvToHtml(args[0], args[1]);
     }
 }
