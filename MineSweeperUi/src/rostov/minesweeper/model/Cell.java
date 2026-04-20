c655e64541029bbe71ae1130eec60ea2d35e163d
RostovGeorgiy <perast201208@gmail.com>
2026-04-20 19:23:06 +0700

    Added separate classes for scores, moved all gui handling to DesktopView class, moved board size handling to MinesweeperModel class, added icons for numbers, changed frame size, added title and icon to frame. Fixed formatting and incorrect names. Added custom Cell class for handling cell properties. Added mose wheel click support, fixed clicks not registering when mouse is moved. Fixed interface moving when flags are placed, incorrect game logic. Added checks for null arguments or inputs.

--- MineSweeperUi/src/rostov/minesweeper/model/Cell.java
+++ MineSweeperUi/src/rostov/minesweeper/model/Cell.java
@@ -5,10 +5,7 @@ public class Cell {
     private boolean isMine;
     private boolean isFlagged;
     private int nearbyMinesAmount;
-
-    public Cell() {
-    }
-
+    
     public boolean getRevealed() {
         return isRevealed;
     }
